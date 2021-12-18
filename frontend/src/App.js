497c4f71d2769074a4bc08b670138bc064789e03
Gabro057 <gabro057@seznam.cz>
2021-12-18 20:22:23 +0100

    Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import logo from './img/logo_w.png';
+import screenshot from "./img/screenshot.png";
 import './App.css';
 
-function App() {
+//const [state, setstate] = useState(initialState)
+
+const Homepage = ({ setScreen }) => {
+  const runActivity = () => {
+    setScreen("addActivity")
+  }
+
   return (
+    <section className="Homepage">
+      <div className="container">
+        <div className="left">
+          <img src={screenshot} className="screenshot" alt="screenshot" />
+        </div>
+        <div className="right">
+          <p>
+            Log your daily activities in this fun to use app!
+          </p>
+          <button onClick={runActivity}>Start</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const AddActivity = () => {
+  return (
+    <section className="AddActivity">
+      <p>Add activity</p>
+    </section>
+  )
+}
+
+const App = () => {
+  const [screen, setScreen] = useState('homepage')
+
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="top">
+        <div className="logo-wrapper">
+          <img src={logo} className="logo" alt="logo" />
+          <div className="logo-text">eDiary</div>
+        </div>
       </header>
+      
+      {screen === 'homepage' && <Homepage setScreen={setScreen} />}
+      {screen === 'addActivity' && <AddActivity />}
+      
+      <footer>
+        <p>© 2021 HrbekJ</p>
+      </footer>
     </div>
   );
 }
